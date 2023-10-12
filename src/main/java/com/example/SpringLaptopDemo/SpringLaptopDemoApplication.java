a9b11d4f72187546a8d742b3e57562d02473347c
Varun Reddy Musku <varunreddymusku@gmail.com>
2023-10-11 22:34:18 -0500

    local files added

---
+++ src/main/java/com/example/SpringLaptopDemo/SpringLaptopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringLaptopDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaptopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaptopDemoApplication.class, args);
+	}
+
+}
